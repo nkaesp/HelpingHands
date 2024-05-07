8602c1944da6f34d94d82b3623323d03dd11392e
Angel Kaye Oporto <oportoak@gmail.com>
2024-05-08 00:38:37 +0800

    OPORTO ga add ug back function pero not all sa mga default ra na back, sa status ug selection kay wala and etc.

--- app/src/main/java/com/intprog/helpinghands/screens/DonationCampaign/TransactionPageActivity.kt
+++ app/src/main/java/com/intprog/helpinghands/screens/DonationCampaign/TransactionPageActivity.kt
@@ -2,11 +2,17 @@ package com.intprog.helpinghands.screens.DonationCampaign
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageButton
 import com.intprog.helpinghands.R
 
 class TransactionPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transaction_page)
+
+        val backTop = findViewById<ImageButton>(R.id.backTop)
+        backTop.setOnClickListener{
+            onBackPressed()
+        }
     }
 }
